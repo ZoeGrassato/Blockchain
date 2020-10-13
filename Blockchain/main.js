e9535446832fd147079ff62a94c60cb1bb6edb62
Zoe Lucinda <zoelucindagrassato@gmail.com>
2020-10-13 17:54:56 +0200

    part 3

--- Blockchain/main.js
+++ Blockchain/main.js
@@ -1,29 +1,39 @@
 //blockchain restricts users from changing/deleting the contents of the blockchain, so if one element is changed, the whole blockchain becomes invalid
 
+//uses pending transactions -> we only create blocks in a specific interval, all blocks that are pending and have not yet been processed, go into a pending transactions array
+//pending transactions go into the Blockchain object itself
+
 
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 //this is a single block
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
+    constructor(timestamp, transactions, previousHash = '') {
         //index -> where it sits in the blockchain
         //data -> data going into the block (details of transaction)
         //previousHash -> hash of the block before this block 
         //ensures integrity of block
-        this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions; //array of transactions
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString(); //we need nonce to make it unique every time
     }
 
     //difficulty increases the computing time needed to meet the requirement of the hash
-    //the more zeros difficulty has, the harder it is to compute
+    //the more zeros the difficulty has, the harder it is to compute
     mineBlock(difficulty) {
         //we create an array with zeros that is exactly the number of difficulty
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
@@ -40,20 +50,35 @@ class Block {
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()]; //the first block (genesis) is always created manually
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, '01/01/2020', 'genesis block', '0');
+        return new Block('01/01/2020', 'genesis block', '0');
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    //-----------replaced by below method--------------
+    //addBlock(newBlock) { 
+    //    newBlock.previousHash = this.getLatestBlock().hash;
+    //    newBlock.mineBlock(this.difficulty); //here we set our blocks hash by mining       with difficulty
+    //    this.chain.push(newBlock);
+    //}
+
+    minePendingTransactions(miningRewardAddress) { //this is to add to the blockchain
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty); //mine for a hash in alignment with our difficulty
+
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward);
+        ]
     }
 
     isChainValid() {
@@ -76,7 +101,7 @@ class Blockchain {
 }
 
 let coin = new Blockchain();
+
 coin.addBlock(new Block(1, '10/10/2020', { amount: 4 }));
 coin.addBlock(new Block(2, '10/9/2020', { amount: 1 }));
 
-console.log(JSON.stringify(coin, null, 4));
